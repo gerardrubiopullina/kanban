aa1abbee4c3f8d5f75e017bf27d62f4f6cdf3279
Gerard <gerard.rubio2000@gmail.com>
2024-12-11 13:44:34 +0100

    drag and drop

--- src/context/TasksContext.tsx
+++ src/context/TasksContext.tsx
@@ -6,6 +6,7 @@ interface TasksContextType {
     tasks: Task[];
     addTask: (title: string, description?: string) => void;
     moveTask: (taskId: number, newStatus: TaskStatus) => void;
+    reorderTasks: (taskId: number, newStatus: TaskStatus, newIndex: number) => void;
 }
 
 export const TasksContext = createContext<TasksContextType | undefined>(undefined);
